13b422aa4abe74e4a4c2fde435dde070384498e0
KenJamesHioki <kenj.hioki@gmail.com>
2024-06-19 12:52:25 +0900

    [change]カスタムフック内のリファクタリング
    不必要なエラーハンドリングがあったため

--- src/hooks/useWantToWatchMovieIds.ts
+++ src/hooks/useWantToWatchMovieIds.ts
@@ -5,41 +5,40 @@ import { showAlert } from "../lib/showAlert";
 import { useTheme } from "../lib/ThemeProvider";
 
 export const useWantToWatchMovieIds = (userId: string) => {
-  const [wantToWatchMovieIds, setWantToWatchMovieIds] = useState<Array<string>>([]);
+  const [wantToWatchMovieIds, setWantToWatchMovieIds] = useState<Array<string>>(
+    []
+  );
   const [isLoading, setIsLoading] = useState(true);
-  const {theme} = useTheme();
+  const { theme } = useTheme();
 
   useEffect(() => {
-    const fetchWantToWatchMovieIds = () => {
+    const listenWantToWatchMovieIds = () => {
       setIsLoading(true);
-      try {
-        const unSub = onSnapshot(
-          doc(db, "wantToWatch", userId),
-          (doc) => {
-            if (doc.exists()) {
-              setWantToWatchMovieIds(doc.data().wantToWatchMovies);
-            } else {
-              setWantToWatchMovieIds([]);
-            }
-            setIsLoading(false);
-          },
-          (error: any) => {
-            showAlert({type: "error" , message: "サーバーとの接続が切断されました", theme})
-            console.error(error.message);
-            setIsLoading(false);
+      const unSub = onSnapshot(
+        doc(db, "wantToWatch", userId),
+        (doc) => {
+          if (doc.exists()) {
+            setWantToWatchMovieIds(doc.data().wantToWatchMovies);
+          } else {
+            setWantToWatchMovieIds([]);
           }
-        );
+          setIsLoading(false);
+        },
+        (error: any) => {
+          showAlert({
+            type: "error",
+            message: "読み込みに失敗しました",
+            theme,
+          });
+          console.error(error.message);
+          setIsLoading(false);
+        }
+      );
 
-        return () => unSub();
-      } catch (error: any) {
-        showAlert({type: "error" , message: "読み込みに失敗しました", theme})
-        console.error(error.meesage);
-      } finally {
-        setIsLoading(false);
-      }
+      return () => unSub();
     };
 
-    fetchWantToWatchMovieIds();
+    listenWantToWatchMovieIds();
   }, [userId]);
 
   return { wantToWatchMovieIds, isLoading };
